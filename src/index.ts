eb583dbf3cdf04d849b783cd2f2da48644d972ed
Salvatore Santagati <github@salsan.dev>
2021-11-15 16:38:00 +0100

    style: change to conditional ternary operator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const arr = require('./src/array')
+const arr = require('./array')
 
 interface Options {
   newline: boolean;
@@ -9,10 +9,9 @@ interface Options {
 
 module.exports = function c2Array (str:string, option:Options):string[] {
   let result:string[] = (option.newline) ? arr.newline(str) : [str]
-
-  if (option.removeEmpty) result = arr.removeEmpty(result)
-  if (option.select) result = arr.select(result, option.select)
-  if (option.splitter) result = arr.splitter(result, option.splitter)
+  result = (option.removeEmpty) ? arr.removeEmpty(result) : result
+  result = (option.select) ? arr.select(result, option.select) : result
+  result = (option.splitter) ? arr.splitter(result, option.splitter) : result
 
   return result
 }

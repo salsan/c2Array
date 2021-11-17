90f1aa7b2e3eb2b16b960f6b4833210147d07f35
Salvatore Santagati <github@salsan.dev>
2021-11-17 18:01:04 +0100

    feat: improve options and add function to module exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,47 @@
-const arr = require('./array')
+const fn = require('./array')
 
 interface Options {
   newline: boolean;
   removeEmpty: boolean;
   select: string;
   splitter: string;
+  removeEmptyString: boolean
 }
 
-module.exports = function c2Array (str:string, option:Options):string[] {
-  let result:string[] = (option.newline) ? arr.newline(str) : [str]
-  result = (option.removeEmpty) ? arr.removeEmpty(result) : result
-  result = (option.select) ? arr.select(result, option.select) : result
-  result = (option.splitter) ? arr.splitter(result, option.splitter) : result
+module.exports = function c2Array (str:string, options:Options):string[] {
+  const defaultOptions = {
+    newline: true,
+    removeEmpty: true,
+    removeEmptyString: false
+  }
+
+  options = Object.assign({}, defaultOptions, options)
+
+  let result:string[] = (options.newline) ? fn.newline(str) : [str]
+  result = (options.removeEmpty) ? fn.removeEmpty(result) : result
+  result = (options.select) ? fn.select(result, options.select) : result
+  result = (options.splitter) ? fn.splitter(result, options.splitter) : result
+  result = (options.removeEmptyString) ? fn.removeEmptyString(result) : result
 
   return result
+}
+
+module.exports.newline = function newline (str:string):string[] {
+  return fn.newline(str)
+}
+
+module.exports.removeEmpty = function removeEmpty (arr:string[]):string[][] {
+  return fn.removeEmpty(arr)
+}
+
+module.exports.select = function (arr:string[], chunk:string):string[] {
+  return fn.select(arr, chunk)
+}
+
+module.exports.splitter = function splitter (arr:string[], chunk:string):string[][] {
+  return fn.splitter(arr, chunk)
+}
+
+module.exports.removeEmptyString = function removeEmptyString (item:string[][]):string[][] {
+  return fn.removeEmptyString(item)
 }
